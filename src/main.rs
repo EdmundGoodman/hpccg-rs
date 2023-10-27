0e32f9be765234e6ad46fc06047ab3d55e4dc314
EdmundGoodman <egoodman3141@gmail.com>
2023-10-27 14:04:19 +0100

    Add translation of waxpby

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 mod ddot;
 mod compute_residual;
+mod waxpby;
 
 use ddot::ddot_idiomatic;
 use compute_residual::compute_residual_idiomatic;
+use waxpby::waxpby_idiomatic;
 
 fn main() {
     let vx = vec![1.0,2.0,3.0];
@@ -10,8 +12,11 @@ fn main() {
     let r = ddot_idiomatic(&vx, &vy);
     println!("{:?} . {:?} = {r}", vx, vy);
 
-    let vx = vec![1.0,2.0,3.0];
-    let vy = vec![3.0,2.0,1.0];
     let r = compute_residual_idiomatic(&vx, &vy);
     println!("residual({:?}, {:?}) = {r}", vx, vy);
+
+    let alpha = 4.0;
+    let beta = 5.0;
+    let r = waxpby_idiomatic(alpha, &vx, beta, &vy);
+    println!("waxpby({:?}, {:?}) = {:?}", vx, vy, r);
 }
