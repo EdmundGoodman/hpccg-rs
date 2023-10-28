814f05ac46f5c70365a3b9929d3505188ee4c2a4
EdmundGoodman <egoodman3141@gmail.com>
2023-10-28 03:08:32 +0100

    Staging before writing unit tests

--- src/hpc_sparse_matrix.rs
+++ src/hpc_sparse_matrix.rs
@@ -2,8 +2,8 @@ use std::rc::Rc;
 use std::cell::RefCell;
 
 #[derive(Debug)]
-struct HpcSparseMatrix<'a> {
-    title: &'a str,
+struct HpcSparseMatrix { //<'a> {
+    // title: &'a str,
     start_row: i32,
     stop_row: i32,
     total_nrow: i32,
@@ -20,19 +20,8 @@ struct HpcSparseMatrix<'a> {
     list_of_inds: Vec<Rc<RefCell<i32>>>,
 }
 
-/**
- * @brief Generates the inital mesh and values
- *
- * @param nx Size of x dimension
- * @param ny Size of y dimension
- * @param nz Size of z dimension
- * @param A Sparse matrix
- * @param x Inital guess for the mesh
- * @param b Right hand side
- * @param xexact Exact solution (as computed by a direct solver)
- * @param use_7pt_stencil true if using 7 point stencil, otherwise use 27 point stencil
- */
-impl HpcSparseMatrix<'_> {
+
+impl HpcSparseMatrix { //<'_> {
 
     /// Generates the inital mesh and values
     ///
@@ -46,7 +35,7 @@ impl HpcSparseMatrix<'_> {
     ///  * `x` - Inital guess for the mesh
     ///  * `b` - Right hand side
     ///  * `xexact` - Exact solution (as computed by a direct solver)
-    fn generate_matrix(nx: i32, ny: i32, nz: i32) {
+    fn generate_matrix(nx: i32, ny: i32, nz: i32) -> (HpcSparseMatrix, Vec<f64>, Vec<f64>, Vec<f64>) {
         let use_7pt_stencil = false;
 
         // The size of our sub-block (must be non-zero)
@@ -74,9 +63,9 @@ impl HpcSparseMatrix<'_> {
         // Allocate arrays that are of length local_nnz
         // Either, we do reference counting, or we make the data structure less insane
         // let mut list_of_vals: Vec<Rc<RefCell<f64>>> = vec![Rc::new(RefCell::new(0.0)); local_nnz as usize]; //Vec::with_capacity(local_nnz as usize);
-        let mut list_of_vals: Vec<Rc<RefCell<f64>>> = (0..local_nnz).map(|_| Rc::new(RefCell::new(0.0))).collect();
+        let list_of_vals: Vec<Rc<RefCell<f64>>> = (0..local_nnz).map(|_| Rc::new(RefCell::new(0.0))).collect();
         // let mut list_of_inds: Vec<Rc<RefCell<i32>>> = vec![Rc::new(RefCell::new(0)); local_nnz as usize]; //Vec::with_capacity(local_nnz as usize);
-        let mut list_of_inds: Vec<Rc<RefCell<i32>>> = (0..local_nnz).map(|_| Rc::new(RefCell::new(0))).collect();
+        let list_of_inds: Vec<Rc<RefCell<i32>>> = (0..local_nnz).map(|_| Rc::new(RefCell::new(0))).collect();
 
         let mut curvalind: usize = 0;
         let mut curindind: usize = 0;
@@ -85,7 +74,6 @@ impl HpcSparseMatrix<'_> {
             for iy in 0..ny {
                 for ix in 0..nx {
 
-                    let curlocalrow = (iz*nx*ny+iy*nx+ix) as usize;
                     let currow = start_row+iz*nx*ny+iy*nx+ix;
                     let mut nnzrow = 0;
                     ptr_to_vals_in_row.push(Rc::clone(&list_of_vals[curvalind]));
@@ -125,26 +113,9 @@ impl HpcSparseMatrix<'_> {
             }
         }
 
-        println!("Matrix has {local_nrow} rows, with {local_nnz} non-zero values");
-
-        println!("\n\n");
-        println!("list_of_vals: {:?}", list_of_vals);
-        println!("list_of_inds: {:?}", list_of_inds);
-        // println!("list_of_inds item: {:?}", Rc::clone(&list_of_inds[2]));
-        println!("\n");
-        println!("nnz_in_row: {:?}", nnz_in_row);
-        println!("ptr_to_vals_in_row: {:?}", ptr_to_vals_in_row);
-        println!("ptr_to_inds_in_row: {:?}", ptr_to_inds_in_row);
-        println!("ptr_to_diags: {:?}", ptr_to_diags);
-        println!("\n\n");
-
-        println!("x: {:?}", x);
-        println!("b: {:?}", b);
-        println!("xexact: {:?}", xexact);
-
-        let binding = String::from("Sparse matrix");
+        // let binding = String::from("Sparse matrix");
         let A = HpcSparseMatrix {
-            title: binding.as_str(),
+            // title: binding.as_str(),
             start_row,
             stop_row,
             local_nrow,
@@ -153,20 +124,50 @@ impl HpcSparseMatrix<'_> {
             local_ncol: local_nrow,
             local_nnz,
             nnz_in_row,
-            ptr_to_vals_in_row: ptr_to_vals_in_row.clone(),
-            ptr_to_inds_in_row: ptr_to_inds_in_row.clone(),
-            ptr_to_diags: ptr_to_diags.clone(),
-            list_of_vals: list_of_vals.clone(),
-            list_of_inds: list_of_inds.clone(),
+            ptr_to_vals_in_row: ptr_to_vals_in_row,
+            ptr_to_inds_in_row: ptr_to_inds_in_row,
+            ptr_to_diags: ptr_to_diags,
+            list_of_vals: list_of_vals,
+            list_of_inds: list_of_inds,
         };
 
-        // println!("{:?}", A)
-
+        (A, x, b, xexact)
     }
 }
 
 
 #[test]
 fn test_sparse_matrix() {
-    HpcSparseMatrix::generate_matrix(5, 5, 5);
+    let (A, x, b, xexact) = HpcSparseMatrix::generate_matrix(2, 2, 2);
+    // println!("Matrix has {} rows, with {} non-zero values", A.local_nrow, A.local_nnz);
+    //
+    // println!("\n\n");
+    // println!("list_of_vals: {:?}", A.list_of_vals);
+    // println!("list_of_inds: {:?}", A.list_of_inds);
+    // println!("list_of_inds length: {}", A.list_of_inds.len());
+    // println!("\n");
+    // println!("nnz_in_row: {:?}", A.nnz_in_row);
+    // println!("ptr_to_vals_in_row: {:?}", A.ptr_to_vals_in_row);
+    // println!("ptr_to_inds_in_row: {:?}", A.ptr_to_inds_in_row);
+    // println!("ptr_to_diags: {:?}", A.ptr_to_diags);
+    // println!("\n\n");
+    //
+    // println!("x: {:?}", x);
+    // println!("b: {:?}", b);
+    // println!("xexact: {:?}", xexact);
+
+    assert_eq!(A.local_nrow,8);
+    assert_eq!(A.local_nnz,216);
+    // .. And so on for all the expected values
+    /*
+    nnz_in_row (8): [8, 8, 8, 8, 8, 8, 8, 8, ]
+    ptr_to_vals_in_row (8): [27, -1, -1, -1, -1, -1, -1, -1, ]
+    ptr_to_inds_in_row (8): [0, 0, 0, 0, 0, 0, 0, 0, ]
+    ptr_to_diags (8): [27, 27, 27, 27, 27, 27, 27, 27, ]
+    list_of_vals (216): [27, -1, -1, -1, -1, -1, -1, -1, -1, 27, -1, -1, -1, -1, -1, -1, -1, -1, 27, -1, -1, -1, -1, -1, -1, -1, -1, 27, -1, -1, -1, -1, -1, -1, -1, -1, 27, -1, -1, -1, -1, -1, -1, -1, -1, 27, -1, -1, -1, -1, -1, -1, -1, -1, 27, -1, -1, -1, -1, -1, -1, -1, -1, 27, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, ]
+    list_of_inds (216): [0, 1, 2, 3, 4, 5, 6, 7, 0, 1, 2, 3, 4, 5, 6, 7, 0, 1, 2, 3, 4, 5, 6, 7, 0, 1, 2, 3, 4, 5, 6, 7, 0, 1, 2, 3, 4, 5, 6, 7, 0, 1, 2, 3, 4, 5, 6, 7, 0, 1, 2, 3, 4, 5, 6, 7, 0, 1, 2, 3, 4, 5, 6, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, ]
+    x (8): [0, 0, 0, 0, 0, 0, 0, 0, ]
+    b (8): [20, 20, 20, 20, 20, 20, 20, 20, ]
+    xexact (8): [1, 1, 1, 1, 1, 1, 1, 1, ]
+     */
 }
