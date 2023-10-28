be666d2cda311fec917500375e393e4590bcb64d
EdmundGoodman <egoodman3141@gmail.com>
2023-10-28 14:03:30 +0100

    Add rustdoc, improve variable names, and finish unit tests

--- src/hpc_sparse_matrix.rs
+++ src/hpc_sparse_matrix.rs
@@ -1,29 +1,46 @@
 use std::rc::Rc;
 use std::cell::RefCell;
 
+/// A data structure representing a sparse matrix mesh
 #[derive(Debug)]
-struct HpcSparseMatrix { //<'a> {
-    // title: &'a str,
+#[allow(dead_code)]
+struct HpcSparseMatrix {
+    /// The row to start generating the matrix from (always `0` in serial mode)
     start_row: i32,
+    /// The row to stop generating the matrix at (always `x*y*z-1` in serial mode)
     stop_row: i32,
+    /// The total volume of the matrix (always equal to `local_nrow` in serial mode)
     total_nrow: i32,
+    /// The total number of non-zeroes (always equal to `local_nnz` in serial mode)
     total_nnz: i32,
+    /// The local volume of the matrix, calculated as `x*y*z` in serial mode
     local_nrow: i32,
+    /// A variable only used in MPI mode (set to `local_nrow` in serial mode)
     local_ncol: i32,
+    /// The local number of non-zero values, approximated as `local_nrow*27`
     local_nnz: i32,
+    /// A vector containing the number of non-zeroes in each row
     nnz_in_row: Vec<i64>,
+    // TODO: Could these be cleaned up as raw values?
+    /// A vector of pointers to values
     ptr_to_vals_in_row: Vec<Rc<RefCell<f64>>>,
+    /// A vector of pointers to indices
     ptr_to_inds_in_row: Vec<Rc<RefCell<i32>>>,
+    /// A vector of pointers to diagonals
     ptr_to_diags: Vec<Rc<RefCell<f64>>>,
     // Needed for cleaning up memory (in C++)
+    // TODO: Could these be discarded?
+    /// A vector of values stored in the matrix
     list_of_vals: Vec<Rc<RefCell<f64>>>,
+    /// A vector of indices into the matrix
     list_of_inds: Vec<Rc<RefCell<i32>>>,
 }
 
+// TODO: Conversion from sparse matrix into pure matrix representation
 
-impl HpcSparseMatrix { //<'_> {
+impl HpcSparseMatrix {
 
-    /// Generates the inital mesh and values
+    /// Generates the initial mesh and its associated values
     ///
     /// # Arguments
     ///  * `nx` - Size of x dimension
@@ -31,10 +48,10 @@ impl HpcSparseMatrix { //<'_> {
     ///  * `nz` - Size of z dimension
     ///
     /// # Return values
-    ///  * `A` - Sparse matrix
-    ///  * `x` - Inital guess for the mesh
-    ///  * `b` - Right hand side
-    ///  * `xexact` - Exact solution (as computed by a direct solver)
+    ///  * `matrix` - Generated sparse matrix
+    ///  * `guess` - Inital guess for the mesh
+    ///  * `rhs` - Right hand side
+    ///  * `exact` - Exact solution (as computed by a direct solver)
     fn generate_matrix(nx: i32, ny: i32, nz: i32) -> (HpcSparseMatrix, Vec<f64>, Vec<f64>, Vec<f64>) {
         let use_7pt_stencil = false;
 
@@ -56,9 +73,9 @@ impl HpcSparseMatrix { //<'_> {
         let mut ptr_to_diags: Vec<Rc<RefCell<f64>>> = Vec::with_capacity(local_nrow as usize);
 
         // Output data other than the sparse matrix
-        let mut x : Vec<f64> = Vec::with_capacity(local_nrow as usize);
-        let mut b : Vec<f64> = Vec::with_capacity(local_nrow as usize);
-        let mut xexact: Vec<f64> = Vec::with_capacity(local_nrow as usize);
+        let mut guess : Vec<f64> = Vec::with_capacity(local_nrow as usize);
+        let mut rhs : Vec<f64> = Vec::with_capacity(local_nrow as usize);
+        let mut exact: Vec<f64> = Vec::with_capacity(local_nrow as usize);
 
         // Allocate arrays that are of length local_nnz
         // Either, we do reference counting, or we make the data structure less insane
@@ -106,16 +123,14 @@ impl HpcSparseMatrix { //<'_> {
                         }
                     }
                     nnz_in_row.push(nnzrow);
-                    x.push(0.0);
-                    b.push(27.0 - ((nnzrow-1) as f64));
-                    xexact.push(1.0);
+                    guess.push(0.0);
+                    rhs.push(27.0 - ((nnzrow-1) as f64));
+                    exact.push(1.0);
                 }
             }
         }
 
-        // let binding = String::from("Sparse matrix");
-        let A = HpcSparseMatrix {
-            // title: binding.as_str(),
+        let matrix = HpcSparseMatrix {
             start_row,
             stop_row,
             local_nrow,
@@ -124,50 +139,63 @@ impl HpcSparseMatrix { //<'_> {
             local_ncol: local_nrow,
             local_nnz,
             nnz_in_row,
-            ptr_to_vals_in_row: ptr_to_vals_in_row,
-            ptr_to_inds_in_row: ptr_to_inds_in_row,
-            ptr_to_diags: ptr_to_diags,
-            list_of_vals: list_of_vals,
-            list_of_inds: list_of_inds,
+            ptr_to_vals_in_row,
+            ptr_to_inds_in_row,
+            ptr_to_diags,
+            list_of_vals,
+            list_of_inds,
         };
 
-        (A, x, b, xexact)
+        (matrix, guess, rhs, exact)
     }
 }
 
 
 #[test]
 fn test_sparse_matrix() {
-    let (A, x, b, xexact) = HpcSparseMatrix::generate_matrix(2, 2, 2);
-    // println!("Matrix has {} rows, with {} non-zero values", A.local_nrow, A.local_nnz);
-    //
-    // println!("\n\n");
-    // println!("list_of_vals: {:?}", A.list_of_vals);
-    // println!("list_of_inds: {:?}", A.list_of_inds);
-    // println!("list_of_inds length: {}", A.list_of_inds.len());
-    // println!("\n");
-    // println!("nnz_in_row: {:?}", A.nnz_in_row);
-    // println!("ptr_to_vals_in_row: {:?}", A.ptr_to_vals_in_row);
-    // println!("ptr_to_inds_in_row: {:?}", A.ptr_to_inds_in_row);
-    // println!("ptr_to_diags: {:?}", A.ptr_to_diags);
-    // println!("\n\n");
-    //
-    // println!("x: {:?}", x);
-    // println!("b: {:?}", b);
-    // println!("xexact: {:?}", xexact);
+    let (matrix, guess, rhs, exact) = HpcSparseMatrix::generate_matrix(2, 2, 2);
 
-    assert_eq!(A.local_nrow,8);
-    assert_eq!(A.local_nnz,216);
-    // .. And so on for all the expected values
-    /*
-    nnz_in_row (8): [8, 8, 8, 8, 8, 8, 8, 8, ]
-    ptr_to_vals_in_row (8): [27, -1, -1, -1, -1, -1, -1, -1, ]
-    ptr_to_inds_in_row (8): [0, 0, 0, 0, 0, 0, 0, 0, ]
-    ptr_to_diags (8): [27, 27, 27, 27, 27, 27, 27, 27, ]
-    list_of_vals (216): [27, -1, -1, -1, -1, -1, -1, -1, -1, 27, -1, -1, -1, -1, -1, -1, -1, -1, 27, -1, -1, -1, -1, -1, -1, -1, -1, 27, -1, -1, -1, -1, -1, -1, -1, -1, 27, -1, -1, -1, -1, -1, -1, -1, -1, 27, -1, -1, -1, -1, -1, -1, -1, -1, 27, -1, -1, -1, -1, -1, -1, -1, -1, 27, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, ]
-    list_of_inds (216): [0, 1, 2, 3, 4, 5, 6, 7, 0, 1, 2, 3, 4, 5, 6, 7, 0, 1, 2, 3, 4, 5, 6, 7, 0, 1, 2, 3, 4, 5, 6, 7, 0, 1, 2, 3, 4, 5, 6, 7, 0, 1, 2, 3, 4, 5, 6, 7, 0, 1, 2, 3, 4, 5, 6, 7, 0, 1, 2, 3, 4, 5, 6, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, ]
-    x (8): [0, 0, 0, 0, 0, 0, 0, 0, ]
-    b (8): [20, 20, 20, 20, 20, 20, 20, 20, ]
-    xexact (8): [1, 1, 1, 1, 1, 1, 1, 1, ]
-     */
+    assert_eq!(matrix.local_nrow, 8);
+    assert_eq!(matrix.local_nnz, 216);
+    assert_eq!(matrix.nnz_in_row, vec![8; 8]);
+
+    let vals_in_row: Vec<f64> = matrix.ptr_to_vals_in_row.iter().map(|x| *x.borrow()).collect();
+    let inds_in_row: Vec<i32> = matrix.ptr_to_inds_in_row.iter().map(|x| *x.borrow()).collect();
+    let diags: Vec<f64> = matrix.ptr_to_diags.iter().map(|x| *x.borrow()).collect();
+    assert_eq!(vals_in_row, vec![27.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0]);
+    assert_eq!(inds_in_row, vec![0; 8]);
+    assert_eq!(diags, vec![27.0; 8]);
+
+    let vals: Vec<f64> = matrix.list_of_vals.iter().map(|x| *x.borrow()).collect();
+    let expected_vals =
+        vec![27.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, 27.0, -1.0, -1.0, -1.0, -1.0,
+             -1.0, -1.0, -1.0, -1.0, 27.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, 27.0,
+             -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, 27.0, -1.0, -1.0, -1.0, -1.0, -1.0,
+             -1.0, -1.0, -1.0, 27.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, 27.0, -1.0,
+             -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, 27.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+             0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+             0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+             0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+             0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+             0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+             0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+             0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+             0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+             0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0];
+    let inds: Vec<i32> = matrix.list_of_inds.iter().map(|x| *x.borrow()).collect();
+    let expected_inds =
+        vec![0, 1, 2, 3, 4, 5, 6, 7, 0, 1, 2, 3, 4, 5, 6, 7, 0, 1, 2, 3, 4, 5, 6, 7, 0, 1, 2, 3, 4,
+             5, 6, 7, 0, 1, 2, 3, 4, 5, 6, 7, 0, 1, 2, 3, 4, 5, 6, 7, 0, 1, 2, 3, 4, 5, 6, 7, 0, 1,
+             2, 3, 4, 5, 6, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    assert_eq!(vals, expected_vals);
+    assert_eq!(inds, expected_inds);
+
+    assert_eq!(guess, vec![0.0; 8]);
+    assert_eq!(rhs, vec![20.0; 8]);
+    assert_eq!(exact, vec![1.0; 8]);
 }
