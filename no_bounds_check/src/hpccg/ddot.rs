5653c5517396973dbea5b2051131b11b9661bfe4
EdmundGoodman <egoodman3141@gmail.com>
2024-01-04 20:30:37 +0000

    Add version without bounds checking

---
+++ no_bounds_check/src/hpccg/ddot.rs
@@ -0,0 +1,37 @@
+/// A method to compute the dot product of two vectors.
+///
+/// This function optimises caching by only accessing one of the vectors if both of the
+/// input values point to the same vector.
+///
+/// # Arguments
+/// * `width` - The width of both input vectors.
+/// * `lhs` - The first input vector.
+/// * `rhs` - The second input vector.
+pub fn ddot(width: usize, lhs: &[f64], rhs: &[f64]) -> f64 {
+    let mut result: f64 = 0.0;
+    if std::ptr::eq(lhs, rhs) {
+        debug_assert!(lhs.len() == width);
+        for i in 0..width {
+            // result += lhs[i] * lhs[i];
+            result += unsafe { lhs.get_unchecked(i) * lhs.get_unchecked(i) };
+        }
+    } else {
+        debug_assert!(lhs.len() == width && rhs.len() == width);
+        for i in 0..width {
+            // result += lhs[i] * rhs[i];
+            result += unsafe { lhs.get_unchecked(i) * rhs.get_unchecked(i) };
+        }
+    }
+    result
+}
+
+#[test]
+fn test_ddot() {
+    let width = 3;
+    let lhs = vec![1.0, 2.0, 3.0];
+    let rhs = vec![3.0, 2.0, 1.0];
+    let result = ddot(width, &lhs, &rhs);
+    assert_eq!(result, 10.0);
+    let result = ddot(width, &lhs, &lhs);
+    assert_eq!(result, 14.0);
+}
---
+++ no_bounds_check/src/hpccg/sparsmv.rs
@@ -0,0 +1,55 @@
+use super::SparseMatrix;
+
+/// Sparse matrix-vector multiplication
+///
+/// # Arguments
+/// * `matrix` - A representation of a sparse matrix.
+/// * `vector` - The input vector to multiply the sparse matrix by.
+pub fn sparsemv(matrix: &SparseMatrix, vector: &[f64]) -> Vec<f64> {
+    let nrow = matrix.local_nrow;
+
+    let mut result = Vec::with_capacity(vector.len());
+    for i in 0..nrow {
+        let mut sum = 0.0;
+
+        debug_assert!(nrow <= matrix.ptr_to_vals_in_row.len());
+        debug_assert!(nrow <= matrix.ptr_to_inds_in_row.len());
+        debug_assert!(nrow <= matrix.nnz_in_row.len());
+        let start_val_ind = unsafe { matrix.ptr_to_vals_in_row.get_unchecked(i) };
+        let start_ind_ind = unsafe { matrix.ptr_to_inds_in_row.get_unchecked(i) };
+        let cur_nnz = unsafe { *matrix.nnz_in_row.get_unchecked(i) };
+        debug_assert!(start_val_ind + cur_nnz <= matrix.list_of_vals.len());
+        debug_assert!(start_ind_ind + cur_nnz <= matrix.list_of_inds.len());
+        for j in 0..cur_nnz {
+            sum += unsafe {
+                matrix.list_of_vals.get_unchecked(start_val_ind + j)
+                    * vector.get_unchecked(*matrix.list_of_inds.get_unchecked(start_ind_ind + j))
+            };
+        }
+
+        result.push(sum);
+    }
+
+    result
+}
+
+#[test]
+fn test_sparsemv() {
+    let (matrix, _, _, _) = SparseMatrix::generate_matrix(2, 2, 2);
+    let vx = vec![20.0; 8];
+    let vy = sparsemv(&matrix, &vx);
+    assert_eq!(vy, vec![400.0; 8]);
+
+    let (matrix, _, _, _) = SparseMatrix::generate_matrix(3, 3, 3);
+    let vx = vec![
+        20.0, 16.0, 20.0, 16.0, 10.0, 16.0, 20.0, 16.0, 20.0, 16.0, 10.0, 16.0, 10.0, 1.0, 10.0,
+        16.0, 10.0, 16.0, 20.0, 16.0, 20.0, 16.0, 10.0, 16.0, 20.0, 16.0, 20.0,
+    ];
+    let expected_vy = vec![
+        461.0, 287.0, 461.0, 287.0, 21.0, 287.0, 461.0, 287.0, 461.0, 287.0, 21.0, 287.0, 21.0,
+        -385.0, 21.0, 287.0, 21.0, 287.0, 461.0, 287.0, 461.0, 287.0, 21.0, 287.0, 461.0, 287.0,
+        461.0,
+    ];
+    let vy = sparsemv(&matrix, &vx);
+    assert_eq!(vy, expected_vy);
+}
