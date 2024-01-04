361d3bdf3022c06004e42efcca3f15e964936b94
EdmundGoodman <egoodman3141@gmail.com>
2024-01-04 22:57:45 +0000

    Improve remote run script

--- utils/remoterun.sh
+++ utils/remoterun.sh
@@ -3,10 +3,9 @@
 set -e
 
 # Set up for the batch job
-cd ..
-cargo build --release
-cd utils
 mkdir -p joboutputs
+rm -f joboutputs/most_recent.out joboutputs/most_recent.err
+CARGO_TARGET_DIR=./joboutputs/target cargo build --manifest-path=${1:-..}/Cargo.toml --release
 
 # Submit the batch to Kudu, and record its batch number
 BATCH=$( sbatch kudu.sbatch )
@@ -19,9 +18,8 @@ echo "Note: Don't worry if the job is PENDING, the job will be ran as soon as po
 squeue -u $( whoami ) -o "%.8i %.20j %.10T %.5M %.20R %.20e"
 
 # Soft-link to the most recent run for convenience
-rm -f most_recent.out most_recent.err
-ln -s joboutputs/hpccg-rs_$BATCHNO.out most_recent.out
-ln -s joboutputs/hpccg-rs_$BATCHNO.err most_recent.err
+ln -s hpccg-rs_$BATCHNO.out ./joboutputs/most_recent.out
+ln -s hpccg-rs_$BATCHNO.err ./joboutputs/most_recent.err
 
 # Show the estimate to to complete
 #ENDTIME=$( squeue -o "%i %e" | grep $BATCHNO | cut -d " " -f 2 )
