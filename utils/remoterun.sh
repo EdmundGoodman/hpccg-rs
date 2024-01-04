379f96c5d219a354e52d20509559a1054ee6791d
EdmundGoodman <egoodman3141@gmail.com>
2024-01-04 22:21:38 +0000

    Fix batch job submission script

--- utils/remoterun.sh
+++ utils/remoterun.sh
@@ -2,10 +2,11 @@
 
 set -e
 
-# Compile the case to run
+# Set up for the batch job
 cd ..
 cargo build --release
 cd utils
+mkdir -p joboutputs
 
 # Submit the batch to Kudu, and record its batch number
 BATCH=$( sbatch kudu.sbatch )
@@ -17,6 +18,11 @@ echo "===== My jobs ====="
 echo "Note: Don't worry if the job is PENDING, the job will be ran as soon as possible."
 squeue -u $( whoami ) -o "%.8i %.20j %.10T %.5M %.20R %.20e"
 
+# Soft-link to the most recent run for convenience
+rm -f most_recent.out most_recent.err
+ln -s joboutputs/hpccg-rs_$BATCHNO.out most_recent.out
+ln -s joboutputs/hpccg-rs_$BATCHNO.err most_recent.err
+
 # Show the estimate to to complete
-ENDTIME=$( squeue -o "%i %e" | grep $BATCHNO | cut -d " " -f 2 )
-echo "===== Job $BATCHNO has been submitted, should be finished by $ENDTIME ====="
+#ENDTIME=$( squeue -o "%i %e" | grep $BATCHNO | cut -d " " -f 2 )
+#echo "===== Job $BATCHNO has been submitted, should be finished by $ENDTIME ====="
