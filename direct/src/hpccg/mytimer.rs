8d40fefd1646fb76b2f0c2ae167a5826538fd7a3
EdmundGoodman <egoodman3141@gmail.com>
2024-01-04 15:39:59 +0000

    Save naive direct translation

---
+++ direct/src/hpccg/mytimer.rs
@@ -0,0 +1,39 @@
+use std::time::{SystemTime, UNIX_EPOCH};
+use std::mem::MaybeUninit;
+
+/// Alias function to allow switching timers.
+pub fn mytimer() -> f64 {
+    // wall_mytimer()
+    // sysconf_mytimer()
+    getrusage_mytimer()
+}
+
+/// A function to get the wall clock time in seconds since the UNIX epoch.
+pub fn wall_mytimer() -> f64 {
+    let tp = SystemTime::now()
+        .duration_since(UNIX_EPOCH).unwrap();
+    let seconds = tp.as_secs() as f64;
+    let micro_seconds = tp.subsec_micros() as f64;
+    seconds + micro_seconds / 1_000_000.0
+}
+
+/// A function to get the system clock time in seconds.
+pub fn sysconf_mytimer() -> f64 {
+    unsafe {
+        let mut ts: MaybeUninit<libc::tms> = MaybeUninit::uninit();
+        let clock_tick = libc::sysconf(libc::_SC_CLK_TCK) as f64;
+        libc::times(ts.as_mut_ptr());
+        (ts.assume_init().tms_utime as f64) / clock_tick
+    }
+}
+
+/// A function to get the CPU time (user and system) in seconds.
+pub fn getrusage_mytimer() -> f64 {
+    unsafe {
+        let mut ruse: MaybeUninit<libc::rusage> = MaybeUninit::uninit();
+        libc::getrusage(libc::RUSAGE_SELF, ruse.as_mut_ptr());
+        let seconds = ruse.assume_init().ru_utime.tv_sec as f64;
+        let micro_seconds = ruse.assume_init().ru_utime.tv_usec as f64;
+        seconds + micro_seconds / 1_000_000.0
+    }
+}
