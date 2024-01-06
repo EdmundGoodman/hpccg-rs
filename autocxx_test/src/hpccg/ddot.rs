c355c42985c2f588fb29596fd75877edb5790df9
EdmundGoodman <egoodman3141@gmail.com>
2024-01-06 22:53:39 +0000

    Merge rust and cpp unit tests

--- autocxx_test/src/hpccg/ddot.rs
+++ autocxx_test/src/hpccg/ddot.rs
@@ -1,3 +1,12 @@
+use autocxx::prelude::*;
+use autocxx::c_int;
+
+include_cpp! {
+    #include "ddot.hpp"
+    safety!(unsafe_ffi)
+    generate!("ddot")
+}
+
 /// A method to compute the dot product of two vectors.
 ///
 /// This function optimises caching by only accessing one of the vectors if both of the
@@ -21,39 +30,36 @@ pub fn ddot(width: usize, lhs: &[f64], rhs: &[f64]) -> f64 {
     result
 }
 
+
 #[test]
 fn test_ddot() {
     let width = 3;
     let lhs = vec![1.0, 2.0, 3.0];
     let rhs = vec![3.0, 2.0, 1.0];
-    let result = ddot(width, &lhs, &rhs);
-    assert_eq!(result, 10.0);
-    let result = ddot(width, &lhs, &lhs);
-    assert_eq!(result, 14.0);
-}
 
-
-
-
-
-use autocxx::prelude::*;
-use autocxx::c_int;
-
-include_cpp! {
-    #include "ddot.hpp"
-    safety!(unsafe_ffi)
-    generate!("ddot")
-}
-
-#[test]
-fn test_ddot_cpp() {
-    let n = c_int(3);
-    let mut x = vec![ 1.0, 2.0, 3.0 ];
-    let mut y = vec![ 3.0, 2.0, 1.0 ];
-    let mut result = 0.0;
-    let expected = 10.0;
+    let rust_result = ddot(width, &lhs, &rhs);
+    let mut cpp_result = 0.0;
     unsafe {
-        ffi::ddot(n, x.as_mut_ptr(), y.as_mut_ptr(), &mut result);
+        ffi::ddot(
+            c_int(width as i32),
+            lhs.clone().as_mut_ptr(),
+            rhs.clone().as_mut_ptr(),
+            &mut cpp_result
+        );
     }
-    assert_eq!(result, expected);
+    assert_eq!(rust_result, 10.0);
+    assert_eq!(rust_result, cpp_result);
+
+    let rust_result = ddot(width, &lhs, &lhs);
+    let mut cpp_result = 0.0;
+    unsafe {
+        ffi::ddot(
+            c_int(width as i32),
+            lhs.clone().as_mut_ptr(),
+            lhs.clone().as_mut_ptr(),
+            &mut cpp_result
+        );
+    }
+    assert_eq!(rust_result, 14.0);
+    assert_eq!(rust_result, cpp_result);
 }
