77448823bc9a1b6c5890877656a579420d15aaf2
EdmundGoodman <egoodman3141@gmail.com>
2024-01-08 22:48:25 +0000

    Apply tooling and add python gitignore

--- utils/src/remote_run_all.py
+++ utils/src/remote_run_all.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
+"""A script to run a test matrix of hpccg translations on Kudu batch computer."""
 
-from typing import List, Tuple
 from pathlib import Path
 from subprocess import run as subprocess_run
 
-RUN_ARGS: List[str] = [
+RUN_ARGS: list[str] = [
     "50 50 50",
     "100 100 100",
     "150 150 150",
@@ -15,27 +15,53 @@ RUN_ARGS: List[str] = [
     "400 400 400",
 ]
 
-# (directory, build, executable, args)
-RUN_COMMANDS: List[Tuple[Path, str, Path]] = [
-    # (Path(".."), "cargo build --release", Path("./target/release/hpccg-rs")),
+# Data schema: (directory, build, executable, args)
+RUN_COMMANDS: list[tuple[Path, str, Path]] = [
     (Path("../0_original"), "make", Path("./test_HPCCG")),
-    (Path("../1_naive"), "cargo build --release", Path("./target/release/hpccg-rs-naive")),
-    (Path("../2_indexed"), "cargo build --release", Path("./target/release/hpccg-rs-indexed")),
-    (Path("../3_single_indexed"), "cargo build --release", Path("./target/release/hpccg-rs-single-indexed")),
-    (Path("../4_no_bounds_check"), "cargo build --release", Path("./target/release/hpccg-rs-no-bounds-check")),
-    (Path("../5_iterators"), "cargo build --release", Path("./target/release/hpccg-rs-iterators")),
-    (Path("../6_parallel"), "cargo build --release", Path("./target/release/hpccg-rs-parallel")),
+    (
+        Path("../1_naive"),
+        "cargo build --release",
+        Path("./target/release/hpccg-rs-naive"),
+    ),
+    (
+        Path("../2_indexed"),
+        "cargo build --release",
+        Path("./target/release/hpccg-rs-indexed"),
+    ),
+    (
+        Path("../3_single_indexed"),
+        "cargo build --release",
+        Path("./target/release/hpccg-rs-single-indexed"),
+    ),
+    (
+        Path("../4_no_bounds_check"),
+        "cargo build --release",
+        Path("./target/release/hpccg-rs-no-bounds-check"),
+    ),
+    (
+        Path("../5_iterators"),
+        "cargo build --release",
+        Path("./target/release/hpccg-rs-iterators"),
+    ),
+    (
+        Path("../6_parallel"),
+        "cargo build --release",
+        Path("./target/release/hpccg-rs-parallel"),
+    ),
 ]
 
+
 def main() -> None:
+    """Run the test matrix."""
     for i, args in enumerate(RUN_ARGS):
         print(f"{i+1}) Running with arguments: {args}")
         for j, (directory, build, executable) in enumerate(RUN_COMMANDS):
             command: str = f"cd {directory} && {build} && time {executable} {args}"
             print(f"\t{j+1}) `{command}`")
-            subprocess_run(["./remoterun.sh", command])
+            subprocess_run(["./remoterun.sh", command])  # noqa: S603
             print("")
         print("\n")
+
 
 if __name__ == "__main__":
     main()
